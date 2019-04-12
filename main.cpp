52f32f06a90e5fc7cb18310ba5f27612324a6752
Alexander Davi <alexander.davi@hibou.polytechnique.fr>
2019-04-12 16:48:01 +0200

    t3

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@ int main(){
    
    std::cout<< "Hello world! coucou" << std::endl;
    std::cout<<"Goodbye world!<<std::endl;
+   
+   for(int i=0;i<10;i++){
+      std::cout<<i<<std::endl;
+   }
    return 0;
    
 }
